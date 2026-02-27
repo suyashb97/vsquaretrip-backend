fb1dafbb712b712bf727732bedf40557122151fa
suyashb97 <suyashbhawsar97@gmail.com>
2026-02-27 17:04:39 +0530

    fix previous code slug

--- api/update-slugs.js
+++ api/update-slugs.js
@@ -1,23 +1,37 @@
 import connectDB from "../utils/connect.js";
 import Package from "../models/Package.js";
 
-const handler = async (req, res) => {
-  await connectDB();
+export default async function handler(req, res) {
+  try {
+    await connectDB();
 
-  const packages = await Package.find();
+    const packages = await Package.find();
 
-  for (let pkg of packages) {
-    if (!pkg.slug) {
-      pkg.slug = pkg.name
-        .toLowerCase()
-        .replace(/ /g, "-")
-        .replace(/[^\w-]+/g, "");
+    for (let pkg of packages) {
+      if (!pkg.slug) {
 
-      await pkg.save();
+        let baseSlug = pkg.name
+          .toLowerCase()
+          .replace(/ /g, "-")
+          .replace(/[^\w-]+/g, "");
+
+        let slug = baseSlug;
+        let count = 1;
+
+        // check duplicate slug
+        while (await Package.findOne({ slug })) {
+          slug = `${baseSlug}-${count}`;
+          count++;
+        }
+
+        pkg.slug = slug;
+        await pkg.save();
+      }
     }
+
+    res.json({ message: "Slugs Updated Successfully" });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-
-  res.json({ message: "Slugs Updated" });
-};
-
-export default handler;
+}
