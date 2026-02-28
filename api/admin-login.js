3824337b186cc7e5e8573a8d19b61056f388c5cd
suyashb97 <suyashbhawsar97@gmail.com>
2026-02-28 16:47:10 +0530

    update admin-login.js

--- api/admin-login.js
+++ api/admin-login.js
@@ -1,7 +1,45 @@
+// import connectDB from "../utils/connect.js";
+// import User from "../models/User.js";
+// import bcrypt from "bcryptjs";
+// import jwt from "jsonwebtoken";
+// import { withCors } from "../utils/withCors.js";
+
+// async function handler(req, res) {
+//   if (req.method !== "POST")
+//     return res.status(405).json({ message: "Method not allowed" });
+
+//   try {
+//     await connectDB();
+//     const { email, password } = req.body;
+
+//     const admin = await User.findOne({ email });
+//     if (!admin) return res.status(401).json({ message: "Invalid credentials" });
+
+//     const isMatch = await bcrypt.compare(password, admin.password);
+//     if (!isMatch) return res.status(401).json({ message: "Invalid credentials" });
+
+//     const token = jwt.sign(
+//       { id: admin._id, role: admin.role },
+//       process.env.JWT_SECRET,
+//       { expiresIn: "7d" }
+//     );
+
+//     res.status(200).json({ message: "Login successful", token });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: "Server error" });
+//   }
+// }
+
+// export default withCors(handler);
+
+
+
 import connectDB from "../utils/connect.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import cookie from "cookie";
 import { withCors } from "../utils/withCors.js";
 
 async function handler(req, res) {
@@ -13,10 +51,12 @@ async function handler(req, res) {
     const { email, password } = req.body;
 
     const admin = await User.findOne({ email });
-    if (!admin) return res.status(401).json({ message: "Invalid credentials" });
+    if (!admin)
+      return res.status(401).json({ message: "Invalid credentials" });
 
     const isMatch = await bcrypt.compare(password, admin.password);
-    if (!isMatch) return res.status(401).json({ message: "Invalid credentials" });
+    if (!isMatch)
+      return res.status(401).json({ message: "Invalid credentials" });
 
     const token = jwt.sign(
       { id: admin._id, role: admin.role },
@@ -24,7 +64,21 @@ async function handler(req, res) {
       { expiresIn: "7d" }
     );
 
-    res.status(200).json({ message: "Login successful", token });
+    const isProd = process.env.NODE_ENV === "production";
+
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("adminToken", token, {
+        httpOnly: true,
+        secure: isProd,
+        sameSite: isProd ? "none" : "lax",
+        path: "/",
+        maxAge: 60 * 60 * 24 * 7,
+      })
+    );
+
+    res.status(200).json({ message: "Login successful" });
+
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server error" });
