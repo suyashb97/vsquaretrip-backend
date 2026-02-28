04cdd34dd4bd1e31c4684eee8845f53648f18a5c
suyashb97 <suyashbhawsar97@gmail.com>
2026-02-28 16:39:20 +0530

    fix import problem in admin-check.js

--- api/admin-check.js
+++ api/admin-check.js
@@ -1,5 +1,5 @@
-import { verifyAdmin } from "../utils/verifyAdmin.js";
-import { withCors } from "../utils/withCors.js";
+import { verifyAdmin } from "../utils/verifyAdmin";
+import { withCors } from "../utils/withCors";
 
 async function handler(req, res) {
   return res.status(200).json({ message: "Authenticated" });
